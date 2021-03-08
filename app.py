5f025e135a236142f60916db14d25881c0551c92
Anandha Murthy B <anandhamurthy2000@gmail.com>
2021-03-08 17:05:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask, jsonify
 import pickle
-import werkzeug
-import flask
-import scipy.misc
-from PIL import Image
 
-filename = 'ph_model.pkl'
+filename = 'crop_model.pkl'
 model = pickle.load(open(filename, 'rb'))
 
 app = Flask(__name__)
@@ -23,28 +19,11 @@ def getDetails(ph_value):
         description=desc
     )
 
-@app.route('/',methods=['GET','POST'])
-def predict():
-    imagefile = flask.request.files['image0']
-    filename = werkzeug.utils.secure_filename(imagefile.filename)
-    print("\nReceived image File name : " + imagefile.filename)
-    imagefile.save(filename)
-    im = Image.open(filename)
-    immat = im.load()
-    (X, Y) = im.size
-    l=[]
-    avg=0
-    x,y=X-(X//2),Y-(Y//2)
-    l.append([x,y])
-    l.append([x-(X//2)//2,y-(Y//2)//2])
-    l.append([x+(X//2)//2,y-(Y//2)//2])
-    l.append([x-(X//2)//2,y+(Y//2)//2])
-    l.append([x+(X//2)//2,y+(Y//2)//2])
-    image_rgb = im.convert("RGB")
-    for i in l:
-        rgb_pixel_value = image_rgb.getpixel((i[0],i[1]))
-        avg+=model.predict([[rgb_pixel_value[0],rgb_pixel_value[1],rgb_pixel_value[2]]])
-    return getDetails((avg/5)[0])
+@app.route('/predict/<msg>',methods=['GET','POST'])
+def predict(msg):
+    l=msg.split(',')
+    params = [[l[0], l[1], l[2], l[3]]]
+    return model.predict(params)
 
 if __name__ == '__main__':
     app.run(debug=True)
