a32635eb472c1c338a55efce5f77a75ee0cd061e
Anandha Murthy B <anandhamurthy2000@gmail.com>
2021-03-08 21:58:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,31 @@ import pickle
 filename = 'crop_model.pkl'
 model = pickle.load(open(filename, 'rb'))
 
-result={'Rice': 1,
- 'Maize': 2}
-
 app = Flask(__name__)
 
-def getDetails(crop_id):
-    
-    return jsonify(
-        crop_name=list(result.keys())[crop_id-1],
-        temperature='20 to 27',
-        irrigation_pattern='5',
-        disease='Bacterial leaf streak, Rice Bacterial blight, Leaf scald, Bakanae, Brown spot, Stem borers, Rice mealy bugs , Rice gall midge, Rice case worm'
-    )
+def getDetails(predict_list):
+    crop_1 = {
+        "crop_name": "Rice",
+        "probability": predict_list[0],
+        "temperature": '20 to 27',
+        "irrigation_pattern": '5',
+        "disease": 'Bacterial leaf streak, Rice Bacterial blight, Leaf scald, Bakanae, Brown spot, Stem borers, Rice mealy bugs , Rice gall midge, Rice case worm'
+    }
+
+    crop_2 = {
+        "crop_name": "Maize",
+        "probability": predict_list[1],
+        "temperature": '20 to 27',
+        "irrigation_pattern": '5',
+        "disease": 'Bacterial leaf streak, Rice Bacterial blight, Leaf scald, Bakanae, Brown spot, Stem borers, Rice mealy bugs , Rice gall midge, Rice case worm'
+    }
+    response = {
+        'status': 200,
+        'message': 'OK',
+        'crop_1': crop_1,
+        'crop_2': crop_2
+    }
+    return jsonify(response)
 
 @app.route('/predict/',methods=['GET','POST'])
 def predict():
@@ -25,7 +37,8 @@ def predict():
     humidity=request.args.get('humidity', type = float)
     rainfall=request.args.get('rainfall', type = float)
     params = [[ph_value, temperature, humidity, rainfall]]
-    return getDetails(int(model.predict(params)[0]))
+
+    return getDetails(model.predict_proba(params)[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
