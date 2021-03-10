cf8a216485602dc52b3ef1eb733e852d42a713b3
Anandha Murthy B <anandhamurthy2000@gmail.com>
2021-03-10 13:40:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,5 +40,29 @@ def predict():
 
     return getDetails(model.predict_proba(params)[0])
 
+@app.route('/predict_alerts/',methods=['GET','POST'])
+def predict_alerts():
+    api_key = request.args.get('api_key', type=int)
+    longitude = request.args.get('lon', type=float)
+    latitude = request.args.get('lat', type=float)
+    response = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lon='+longitude+'&exclude=hourly,current,minutely&appid='+api_key)
+    data = response.json()
+    daily = list(data['daily'])
+    rain_ids = [500, 501, 502, 503, 504, 511, 520, 521, 522, 531]
+    drizzle_ids = [300, 301, 302, 310, 311, 312, 313, 314, 321]
+    alerts = []
+    for i in daily:
+        if i['weather'][0]['id'] in rain_ids + drizzle_ids:
+            alert = {"dt": i['dt'],
+                     "alert": "There is a " + str(i['weather'][0]['description']).capitalize() + ". You can avoid Irrigating Crops!"}
+            alerts.append(alert)
+    response = {
+        'status': 200,
+        'message': 'OK',
+        'alerts': alerts
+    }
+
+    return jsonify(response)
+
 if __name__ == '__main__':
     app.run(debug=True)
