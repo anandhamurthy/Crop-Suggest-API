2ce7b38993db8e52adae5a1ce8ebc01d8569dd0c
Anandha Murthy B <anandhamurthy2000@gmail.com>
2021-03-10 17:06:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,26 +7,65 @@ model = pickle.load(open(filename, 'rb'))
 app = Flask(__name__)
 
 def getDetails(predict_list):
+
+
     crop_1 = {
         "crop_name": "Rice",
+        "crop_image": "https://firebasestorage.googleapis.com/v0/b/project-farmer-5f498.appspot.com/o/Crop_Images%2Fpaddy.jpg?alt=media&token=2a11d45d-fb5f-4aae-acb6-39f41b625a66",
         "probability": predict_list[0],
-        "temperature": '20 to 27',
-        "irrigation_pattern": '5',
-        "disease": 'Bacterial leaf streak, Rice Bacterial blight, Leaf scald, Bakanae, Brown spot, Stem borers, Rice mealy bugs , Rice gall midge, Rice case worm'
+        "temperature": '20°C to 27°C',
+        "irrigation_pattern": 5,
+        "disease_model": 'url',
+        "disease": '1. Bacterial leaf streak, 2. Rice Bacterial blight, 3. Leaf scald, Bakanae, 4. Brown spot, 5. Stem borers, 6. Rice mealy bugs , 7. Rice gall midge, 8. Rice case worm'
     }
 
     crop_2 = {
         "crop_name": "Maize",
+        "crop_image": "https://firebasestorage.googleapis.com/v0/b/project-farmer-5f498.appspot.com/o/Crop_Images%2Fmaize.jpg?alt=media&token=c0696384-0d1b-4b35-8b4b-0e3704ca4a75",
         "probability": predict_list[1],
-        "temperature": '20 to 27',
-        "irrigation_pattern": '5',
-        "disease": 'Bacterial leaf streak, Rice Bacterial blight, Leaf scald, Bakanae, Brown spot, Stem borers, Rice mealy bugs , Rice gall midge, Rice case worm'
+        "temperature": '21°C to 27°C',
+        "irrigation_pattern": 7,
+        "disease_model": 'url',
+        "disease": '1. Anthracnose , 2. Cercospora leaf spot (Gray leaf spot), 3. Charcoal rot, 4. Common rust, 5. Common smut 6. Downy Mildew'
+    }
+
+    crop_3 = {
+        "crop_name": "Chick Pea",
+        "crop_image": "https://firebasestorage.googleapis.com/v0/b/project-farmer-5f498.appspot.com/o/Crop_Images%2Fchickpea.jpg?alt=media&token=3962ffc6-5c64-4e10-a434-9a6abb08aa44",
+        "probability": predict_list[2],
+        "temperature": 'below 15°C',
+        "irrigation_pattern": 7,
+        "disease_model": 'url',
+        "disease": '1. Fusarium Wilt, 2. Powdery Mildew, 3. White Mold Stem & Crown Rot of Chickpea, 4.	Black Root Rot, 5. Pythium Seedling, 6.	Downy Mildew.Rust'
+    }
+
+    crop_4 = {
+        "crop_name": "Kidney Beans",
+        "crop_image": "https://firebasestorage.googleapis.com/v0/b/project-farmer-5f498.appspot.com/o/Crop_Images%2Fkidneybeans.jpg?alt=media&token=7ee3cff6-01ce-4b7c-bb97-d4cedd50764c",
+        "probability": predict_list[3],
+        "temperature": '15°C to 25°C',
+        "irrigation_pattern": 7,
+        "disease_model": 'url',
+        "disease": '1. Anthracnose, 2. Rust, 3. Root rot, 4. Bean common mosaic disease, 5.	Watery Soft Rot, 6.	Angular leaf spot'
+    }
+
+    crop_5 = {
+        "crop_name": "Pigeon Peas",
+        "crop_image": "https://firebasestorage.googleapis.com/v0/b/project-farmer-5f498.appspot.com/o/Crop_Images%2Fpigeonpeas.png?alt=media&token=5e2ae6bd-fef5-4026-bc04-514b8f12c231",
+        "probability": predict_list[4],
+        "temperature": '26°C to 30°C',
+        "irrigation_pattern": 7,
+        "disease_model": 'url',
+        "disease": '1. Alternaria Blight, 2. Anthracnose, 3. Cercospora leaf sopt, 4. White mold, 5. Wilt, 6. Aphids, 7. Army worms'
     }
     response = {
         'status': 200,
         'message': 'OK',
         'crop_1': crop_1,
-        'crop_2': crop_2
+        'crop_2': crop_2,
+        'crop_3': crop_3,
+        'crop_4': crop_4,
+        'crop_5': crop_5
     }
     return jsonify(response)
 
